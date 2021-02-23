fb5fbce0b3927ff4ec551c869bbb3eae37cb7f0f
khomsan <khomsan_su@cmu.ac.th>
2021-02-23 16:47:11 +0700

    complete

--- lab18_2.cpp
+++ lab18_2.cpp
@@ -5,8 +5,26 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double max(double A,double B){
+	if(A>B){
+		return A;
+	}else
+		return B; 
+}
 
+double min(double A,double B){
+	if(A<B){
+		return A;
+	}else
+		return B;
+}
 
-
+double overlap(Rect *R1,Rect *R2){
+	double  L1 = min(R1->x+R1->w,R2->x+R2->w)-max(R1->x,R2->x);
+	double  L2 = min(R1->y,R2->y)-max(R1->y-R1->h,R2->y-R2->h);
+	if(L1 > 0&&L2>0){
+		return L2*L1; 
+	}else{ 
+	return 0;
+	}
 }
